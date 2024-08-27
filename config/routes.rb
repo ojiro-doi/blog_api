e7cf4c319e00eda1466da21c5740194bfe5efdd2
ojiro-doi <sakura.0046@icloud.com>
2024-08-27 17:49:47 +0900

    add create func

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      # get "posts/index"
+      # get "posts/show"
+      # get "posts/create"
+      # get "posts/update"
+      # get "posts/destroy"
+      resources :posts, only: [ :index, :show, :create, :create, :update, :destroy ]
+    end
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
